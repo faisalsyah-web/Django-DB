b43f9c98a224f7ff527b691a956efba55f301a1b
faisalsyah-web <faisalsyahruddin57@gmail.com>
2022-12-21 20:33:28 +0900

    Tambah App user

--- bandara/urls.py
+++ bandara/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('penerbangan/', include("penerbangan.urls"),)
+    path("admin/", admin.site.urls),
+    path(
+        "penerbangan/",
+        include("penerbangan.urls"),
+    ),
+    path("users/", include("users.urls")),
 ]
--- penerbangan/admin.py
+++ penerbangan/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Penerbangan, Bandara, Penumpang
 
 # Register your models here.
+class PenerbanganAdmin(admin.ModelAdmin):
+    list_display = ("id", "asal", "tujuan", "durasi")
+
+class PenumpangAdmin(admin.ModelAdmin):
+    filter_horizontal = ("dftrpenerbangan", )
+
 admin.site.register(Bandara)
-admin.site.register(Penerbangan)
-admin.site.register(Penumpang)
+admin.site.register(Penerbangan, PenerbanganAdmin)
+admin.site.register(Penumpang, PenumpangAdmin)
